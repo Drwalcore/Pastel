02e213b1c33b9f4a8daed3935fa8683f73f3ea9d
Michał Czerniakowski <mpczerniakowski@gmail.com>
2018-03-24 16:30:42 +0100

    Add empty test case.

--- Example/Tests/ViewControllerSpec.swift
+++ Example/Tests/ViewControllerSpec.swift
@@ -4,30 +4,7 @@ import Nimble_Snapshots
 import Quick
 
 class ViewControllerSpec: QuickSpec {
-
     override func spec() {
-        describe("ViewController") {
-            var sut: ViewController!
-
-            beforeEach {
-                sut = ViewController()
-            }
-
-            afterEach {
-                sut = nil
-            }
-            describe("after view was loaded") {
-                beforeEach {
-                    sut.loadView()
-                }
-            }
-
-            it("shouldHaveValidSnapshot") {
-                sut.view.frame = UIScreen.main.bounds
-
-                expect(sut.view).to(haveValidDeviceAgnosticSnapshot())
-            }
-        }
+        
     }
-
 }
